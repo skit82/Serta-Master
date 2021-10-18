f47adf158dd215162114edf39eefaf3e82125015
Aleksey <a.abrosimow2012@yandex.ru>
2021-10-18 12:10:14 +0300

    Доработки и правки всей страницы

--- email.php
+++ email.php
@@ -1,8 +1,5 @@
-
-
 <?php
-	$back = "<p><a href=\"javascript: history.back()\">Вернуться назад</a></p>";
-
+	
 	if(!empty($_POST['callform-login']) and !empty($_POST['callform-tel']) and !empty($_POST['callform-email'])) {
 		$name = trim(strip_tags($_POST['callform-login']));
 		$phone = trim(strip_tags($_POST['callform-tel']));
@@ -10,7 +7,7 @@
 
 		mail('sertamaster23@yandex.ru', 'Заявка на соотрудничество', 'Вам написал: '.$name.'<br />Его номер: '.$phone.'<br />Его почта: '.$mail);
 
-		echo "Ваше сообщение успешно отправлено!<Br> Вы получите ответ в ближайшее время<Br> $back";
+		echo  "<script>document.location.href='/thanks.php';</script>";
 
 		exit;
 	} 
---
+++ thanks.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Спасибо</title>
+</head>
+<body style="background: rgba(140, 155, 180, 1);">
+		<p style="font-size: 34px;line-height: 40px;font-weight: 600;text-align: center;padding-top: 80px;color: #fff;">Спасибо сообщение успешно отправленно</p>
+		<a style="display: block;font-size: 30px;line-height: 36px;color: #fff;margin: 0 auto;margin-top: 60px;text-align: center;text-decoration: none;" href="https://sertamaster23.ru/">Вернутся назад</a>
+</body>
+</html>
